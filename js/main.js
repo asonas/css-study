b79231f12b559e7bc51c2a5f330d2900a8daf824
asonas <hzw1258@gmail.com>
2012-05-06 20:32:41 +0900

    解説を追記

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
+$(function(){ //ページが読まれたら、中身を実行するよ
+  $('li.parent-menu').click(function(){ // li タグの parent-menu っていう class が"クリック"されたら
+    $('ul.children-menu').toggle(); // ul タグの children-menu を反転するよ
+  })
+})
+
+//toggle()についての説明
 $(function(){
-  $('li.parent-menu').click(function(){
-    console.log('click!');
-    $('ul.children-menu').toggle();
+  $('.dummy').click(function(){
+    //これは
+    $('.dummy-menu').toggle();
+    //以下のように書き換えられる
+    if $('.dummy-menu').attr('class', 'hidden') { // .dummy-menu の class に hidden が含まれていたら
+      $('.dummy-menu').show(); // .dummy-menuを表示
+    } else {
+      $('.dummy-menu').hide(); // .dummy-menuを隠す
+    }
   })
 })
